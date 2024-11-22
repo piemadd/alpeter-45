fc62effefaf0243f02eeeb33bf7e724a3e19cb55
piemadd <piero@piemadd.com>
2024-11-22 12:58:48 -0600

    fixed config

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,16 @@ import FeedGenerator from './server'
 
 const run = async () => {
   dotenv.config()
-  const hostname = maybeStr(process.env.FEEDGEN_HOSTNAME) ?? 'example.com'
-  const serviceDid =
-    maybeStr(process.env.FEEDGEN_SERVICE_DID) ?? `did:web:${hostname}`
+  const hostname = 'alpeter45.piemadd.com';
+  const serviceDid = `did:web:${hostname}`;
   const server = FeedGenerator.create({
-    port: maybeInt(process.env.FEEDGEN_PORT) ?? 3000,
-    listenhost: maybeStr(process.env.FEEDGEN_LISTENHOST) ?? 'localhost',
-    sqliteLocation: maybeStr(process.env.FEEDGEN_SQLITE_LOCATION) ?? ':memory:',
-    subscriptionEndpoint:
-      maybeStr(process.env.FEEDGEN_SUBSCRIPTION_ENDPOINT) ??
-      'wss://bsky.network',
+    port: 3000,
+    listenhost: '0.0.0.0',
+    sqliteLocation: ':memory:',
+    subscriptionEndpoint: 'wss://bsky.network',
     publisherDid:
-      maybeStr(process.env.FEEDGEN_PUBLISHER_DID) ?? 'did:example:alice',
-    subscriptionReconnectDelay:
-      maybeInt(process.env.FEEDGEN_SUBSCRIPTION_RECONNECT_DELAY) ?? 3000,
+      "did:plc:zmvv42wlsta462gsewywlvfl",
+    subscriptionReconnectDelay: 3000,
     hostname,
     serviceDid,
   })
